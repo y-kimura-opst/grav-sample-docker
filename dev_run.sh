eea5827da3e0441afe23165ad974977734f48926
fiio0325 <kimura.y.6033@gmail.com>
2020-12-19 11:45:35 +0900

    pre-install admin plugin

--- dev_run.sh
+++ dev_run.sh
@@ -6,11 +6,5 @@
 rm -fR user
 ln -s _user user
 
-chmod +x bin/*
-
-echo "---> grav plugins installing..."
-bin/gpm install admin -y
-echo "<--- plugins installed."
-
 echo "---> grav start..."
 php -S 0.0.0.0:8080 system/router.php
